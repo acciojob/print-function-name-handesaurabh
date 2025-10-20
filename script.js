870c4f8e094b006d7dd51cebfc19ce94057bae20
handesaurabh <handesaurabh2004@gmail.com>
2025-10-20 19:34:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+functionName(){
+	alert("functionName");
+};
+document.addEventListner("click",functionName);
+
+AccioJob(){
+	alert("AccioJob");
+};
+document.addEventListner("click",AccioJob);
